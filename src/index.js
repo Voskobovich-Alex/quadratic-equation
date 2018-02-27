62513ca9ae58f141d21299d603a88ccded40e35d
alex-voskobovich <36641216+Voskobovich-Alex@users.noreply.github.com>
2018-02-27 18:37:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+var str = equation.split(' ');
+var a = str[0];
+var b = str[3]+str[4];
+var c = str[7]+str[8];
+var x1,x2;
+var diz;
+var rezult = new Array(1,2);
+diz = (b*b) - (4*a*c);
+if (diz >=0) {
+	x1 =(- b + ( Math.sqrt(diz) ) ) / (2 * a);
+	x2 =(- b - ( Math.sqrt(diz) ) ) / (2 * a);
+	rezult[0]=Math.round(x1);
+	rezult[1]=Math.round(x2);
+} if (diz < 0) {
+	rezult[0]=0;
+	rezult[1]=0;
+}
+rezult.sort(function (i, j) {
+    return i - j;
+});
+return rezult;
 }
